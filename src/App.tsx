8ba7a431f920a49a35ed27c7aee89d7beaca5f83
loyoenock <loyoenock@gmail.com>
2024-08-04 15:48:00 +0300

    Inintial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './globals.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello Torah Tale
+    </h1>
+    </div>
+  );
+};
+export default App;
