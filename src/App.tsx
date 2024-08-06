4f15ca640a8ad1535aac875137ca70fb79e880d3
loyoenock <loyoenock@gmail.com>
2024-08-06 08:26:45 +0300

    Private and public routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './globals.css';
+import SigninForm from './_auth/SigninForm/SigninForm';
+import { Home } from './_root/pages';
+import SignupForm from './_auth/SigninForm/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello Torah Tale
-    </h1>
-    </div>
+    <main className='flex h-screen'>
+      <Routes>
+        {/* Public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-in" element={<SignupForm />} />
+        </Route>
+        
+        {/* Private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>        
+      </Routes>
+    </main>
   );
 };
 export default App;
